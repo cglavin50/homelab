daf2d0e55141a78a47d0e743d7e248c3136c66cb
cglavin50 <cooperglavin@gmail.com>
2025-04-22 18:58:10 -0400

    fix typos in bash script

--- adguard/setup_network.sh
+++ adguard/setup_network.sh
@@ -2,16 +2,16 @@
 
 # Spin up ipvlan network (not supported in compose)
 NETWORK_NAME=adguard-vlan
-SUBNET={2:-192.168.1.10/24}
+SUBNET="{2:-192.168.1.10/24}"
 GATEWAY=192.168.1.1
-PARENT_INTERFACE={1:-wlan0}
+PARENT_INTERFACE="{1:-wlan0}"
 
 # only create if DNE
 if ! docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
 	echo "Creating ipvlan network: $NETWORK_NAME"
 	docker network create \
 		-d ipvlan \
-		--subnet="$SUBNET" \ 
+		--subnet="$SUBNET" \
 		--gateway="$GATEWAY" \
 		-o parent="$PARENT_INTERFACE" \
 		"$NETWORK_NAME"
