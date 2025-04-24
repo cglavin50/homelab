6318f1c8b694bc88c1aa8206660bd7c1855c3860
cglavin50 <cooperglavin@gmail.com>
2025-04-24 16:33:08 -0400

    Change adguard to bridge net

--- adguard/setup_network.sh
+++ adguard/setup_network.sh
@@ -2,9 +2,9 @@
 
 # Spin up ipvlan network (not supported in compose)
 NETWORK_NAME=adguard-vlan
-SUBNET="{2:-192.168.1.10/24}"
+SUBNET="${2:-192.168.1.1/24}"
 GATEWAY=192.168.1.1
-PARENT_INTERFACE="{1:-wlan0}"
+PARENT_INTERFACE="${1:-wlan0}"
 
 # only create if DNE
 if ! docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
